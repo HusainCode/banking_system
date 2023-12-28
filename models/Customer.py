1e190fd0b0267cea8f43e79e58747316e6df0152
HusainCode <alshaikhahmed@hotmail.com>
2023-12-27 20:26:42 -0600

    modified the account class

--- models/Customer.py
+++ models/Customer.py
@@ -2,11 +2,12 @@ class Customer:
     """
         Initialize a new Customer with the given details.
     """
-    def __init__(self, d, age, gender, address):
+    def __init__(self, age, gender, address,total_balance):
         self.name = name # Assigning the name of the customer
         self.address = address # Assigning the address of the customer
         self.age = age # Assigning the age of the customer
         self.gender = gender # Assigning the gender of the customer
+        self.total_balance = total_balance
         self.accounts = [] # List to store accounts associated with the customer
 
     def get_customer_name(self):
