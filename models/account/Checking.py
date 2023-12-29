cf65e098156dcd48868c17a7cf111eedc1385f2c
Husain Alshaikahmed <haa38@txtstte.edu>
2023-12-28 21:19:13 -0600

    Add GUI , testing environment, and modify account classes

--- models/account/Checking.py
+++ models/account/Checking.py
@@ -1,3 +1,12 @@
+from models.account.Account import Account
+
+
 class Checking(Account):
     def __init__(self, account_number, customer_name, balance=0):
         super().__init__(account_number, customer_name, balance)
+
+        def update_checking_account():
+            pass
+
+        def overdraft():
+            pass
