66c566f75dd9a5c942310b2fab87401ce161279f
Husain Alshaikahmed <haa38@txtstte.edu>
2024-01-01 00:54:04 -0600

    began developing the Customer, GUI class, and redesigning the Account class and its children

--- models/Customer.py
+++ models/Customer.py
@@ -1,18 +1,28 @@
+import random
+
+
 class Customer:
     """
         Initialize a new Customer with the given details.
     """
-    def __init__(self, age, gender, address,total_balance):
-        self.name = name # Assigning the name of the customer
-        self.address = address # Assigning the address of the customer
-        self.age = age # Assigning the age of the customer
-        self.gender = gender # Assigning the gender of the customer
-        self.total_balance = total_balance
-        self.accounts = [] # List to store accounts associated with the customer
+    used_id = set()
+    id_range_start = 100000
+    id_range_end = 999999
 
-    def get_customer_name(self):
-        return self.name
+    def __init__(self, full_name: str, customer_id: int = 0):
+        self.full_name = full_name  # Assigning the name of the customer
+        self.customer_id = customer_id
 
-    def get_customer_address(self):
-        return self.address
+    @classmethod
+    def generate_unique_id(cls):
+        while True:
+            new_id = random.randint(cls.id_range_start, cls.id_range_end)
+            if new_id not in cls.used_ids:
+                cls.used_ids.add(new_id)
+                return new_id
 
+    def set_customer_name(self, full_name):
+        self.full_name = full_name
+
+        def get_customer_name(self):
+            return self.name
